bcc32f0e0a1692cfde09d8ee2647f31c868d5e4f
Louis Lu <louislucoll@gmail.com>
2025-09-27 16:18:25 +0800

    Lab6 make clean

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -18,7 +18,8 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
+    static final File CAPERS_FOLDER = new File(join(CWD.getPath()).toURI());
+            // null; // TODO Hint: look at the `join`
                                             //      function in Utils
 
     /**
